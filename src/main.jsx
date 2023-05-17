f5284315ddcb1ba7aaa7e98148620d98d28a5c66
Yamilet Jerez <quieromiwww@gmail.com>
2023-05-17 14:48:34 +0200

    -

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-//importar
 import {CssBaseline} from "@mui/material"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
